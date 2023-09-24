c803545528d709b07d8c17e9a05287d7d909080c
natan.nascimento <natanvnascimento@gmail.com>
2023-09-24 02:54:20 -0300

    More readable code

--- lyricist-processor-xml/src/main/java/cafe/adriel/lyricist/processor/xml/internal/ktx/Xml.kt
+++ lyricist-processor-xml/src/main/java/cafe/adriel/lyricist/processor/xml/internal/ktx/Xml.kt
@@ -19,6 +19,9 @@ private const val TAG_STRING_ARRAY = "string-array"
 private const val TAG_PLURALS = "plurals"
 private const val TAG_ITEM = "item"
 
+private val ReplacementExpr = Regex("@string/(\\S+)")
+private var replacements = mutableListOf<String>()
+
 internal fun FileTreeWalk.filterXmlStringFiles(): Sequence<File> =
     filter {
         it.isFile &&
@@ -26,31 +29,82 @@ internal fun FileTreeWalk.filterXmlStringFiles(): Sequence<File> =
             it.parentFile.name.startsWith(VALUES_FOLDER_NAME, ignoreCase = true)
     }
 
-internal fun File.getXmlStrings(): List<Pair<ResourceName, StringResource>> =
-    konsumeXml()
+private typealias ResourceValues = List<Pair<ResourceName, StringResource>>
+
+internal fun File.getXmlStrings(): ResourceValues {
+    replacements = mutableListOf()
+    return konsumeXml()
         .child(TAG_RESOURCES) {
             getPlainStrings() + getStringArrays() + getPlurals()
         }
+        .applyReplacements()
+}
 
-private fun Konsumer.getPlainStrings(): List<Pair<String, StringResource>> =
+private fun ResourceValues.applyReplacements(): ResourceValues {
+    val values = toMutableList()
+
+    // We need to do a late replacements because Konsume XML resolves everything "on-demand"
+    // so trying to replace value of "A" node in "B" while "B" is being resolved is impossible
+    for (replacement in replacements) {
+        val index = values.indexOfFirst { (key, _) -> key == replacement }
+        val (key, resource) = values[index]
+        val replaced: StringResource = when (resource) {
+            is StringResource.PlainString -> resource.copy(
+                value = replaceResourceValueExpr(resource.value, values)
+            )
+            is StringResource.Plurals -> resource.copy(
+                value = resource.value.mapValues { (_, nodeText) ->
+                    replaceResourceValueExpr(nodeText, values)
+                }
+            )
+            is StringResource.StringArray -> resource.copy(
+                value = resource.value.map { nodeText -> replaceResourceValueExpr(nodeText, values) }
+            )
+        }
+
+        values[index] = key to replaced
+    }
+
+    return values
+}
+
+private fun replaceResourceValueExpr(currentValue: String, values: ResourceValues) =
+    ReplacementExpr.replace(currentValue) { match ->
+        val target = match.groupValues.last()
+        // Searches for `<string name="...">`
+        val (_, resource) = values.first { (name, _) -> name == target }
+        (resource as StringResource.PlainString).value
+    }
+
+private fun Konsumer.getPlainStrings(): ResourceValues =
     children(TAG_STRING) {
-        attributes[ATTRIBUTE_NAME] to StringResource.PlainString(
-            value = text()
+        val attr = attributes[ATTRIBUTE_NAME]
+        attr to StringResource.PlainString(
+            value = textWithReplacements(attr)
         )
     }
 
-private fun Konsumer.getStringArrays(): List<Pair<String, StringResource>> =
+private fun Konsumer.getStringArrays(): ResourceValues =
     children(TAG_STRING_ARRAY) {
-        attributes[ATTRIBUTE_NAME] to StringResource.StringArray(
-            value = children(TAG_ITEM) { text() }
+        val attr = attributes[ATTRIBUTE_NAME]
+        attr to StringResource.StringArray(
+            value = children(TAG_ITEM) { textWithReplacements(attr) }
         )
     }
 
-private fun Konsumer.getPlurals(): List<Pair<String, StringResource>> =
+private fun Konsumer.getPlurals(): ResourceValues =
     children(TAG_PLURALS) {
-        attributes[ATTRIBUTE_NAME] to StringResource.Plurals(
+        val attr = attributes[ATTRIBUTE_NAME]
+        attr to StringResource.Plurals(
             value = children(TAG_ITEM) {
-                Quantity.valueOf(attributes[ATTRIBUTE_QUANTITY].uppercase()) to text()
+                Quantity.valueOf(attributes[ATTRIBUTE_QUANTITY].uppercase()) to
+                    textWithReplacements(attr)
             }.toMap()
         )
+    }
+
+private fun Konsumer.textWithReplacements(attr: String): String = text()
+    .also { text ->
+        if (ReplacementExpr.containsMatchIn(text))
+            replacements.add(attr)
     }
