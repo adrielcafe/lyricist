c9fb1edb3bf4ba8d2d3b2228492907b70d41e026
Ali Nasrabadi <nasrabadiam@gmail.com>
2024-07-05 06:23:13 +0330

    Add fa to multi-module strings

--- sample-multi-module/src/main/java/cafe/adriel/lyricist/sample/multimodule/strings/EnUsMultiModuleStrings.kt
+++ sample-multi-module/src/main/java/cafe/adriel/lyricist/sample/multimodule/strings/EnUsMultiModuleStrings.kt
@@ -1,9 +1,8 @@
 package cafe.adriel.lyricist.sample.multimodule.strings
 
-import cafe.adriel.lyricist.LayoutDirection
 import cafe.adriel.lyricist.LyricistStrings
 
-@LyricistStrings(languageTag = "en", layoutDirection = LayoutDirection.Rtl, default = true)
+@LyricistStrings(languageTag = "en", default = true)
 val EnMultiModuleStrings = MultiModuleStrings(
     string = "Hello Compose!"
 )
---
+++ sample-multi-module/src/main/java/cafe/adriel/lyricist/sample/multimodule/strings/FaBrMultiModuleStrings.kt
@@ -0,0 +1,9 @@
+package cafe.adriel.lyricist.sample.multimodule.strings
+
+import cafe.adriel.lyricist.LayoutDirection
+import cafe.adriel.lyricist.LyricistStrings
+
+@LyricistStrings(languageTag = "fa", layoutDirection = LayoutDirection.Rtl)
+val FaMultiModuleStrings = MultiModuleStrings(
+    string = "سلام کامپوز!"
+)
